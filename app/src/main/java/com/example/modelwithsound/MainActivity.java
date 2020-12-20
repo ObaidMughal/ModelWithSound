0af4b63865a48ae0fb2b9123b0a4ec8e45a18fcc
OBAID MUGHAL <obaidmughal55@gmail.com>
2020-12-20 10:06:02 +0500

    Augmented Reality

--- app/src/main/java/com/example/modelwithsound/MainActivity.java
+++ app/src/main/java/com/example/modelwithsound/MainActivity.java
@@ -9,12 +9,21 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.SkeletonNode;
 import com.google.ar.sceneform.animation.ModelAnimator;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.AnimationData;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+
+import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,19 +31,62 @@ public class MainActivity extends AppCompatActivity {
     private int i =0;
     private SoundPool soundPool;
     private int sound;
+    private Boolean modelPlaced=false;
+    private ArFragment arFragment;
+    private Button startBtn;
+    private ModelRenderable renderable;
+    private Anchor anchor;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArFragment arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
-        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+//        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+//
+//            createModel(hitResult.createAnchor(),arFragment);
+//
+//        });
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdate);
+        startBtn = findViewById(R.id.startBtn);
 
-            createModel(hitResult.createAnchor(),arFragment);
 
-        });
         loadSoundPool();
     }
+
+    private void onUpdate(FrameTime frameTime) {
+
+//        if (modelPlaced)
+//            return;
+
+        Frame frame=arFragment.getArSceneView().getArFrame();
+        Collection<Plane> planes=frame.getUpdatedTrackables(Plane.class);
+
+        for (Plane plane:planes){
+
+            if (plane.getTrackingState()== TrackingState.TRACKING){
+
+                anchor = plane.createAnchor(plane.getCenterPose());
+
+
+                startBtn.setOnClickListener(v -> {
+
+                    if(modelPlaced)
+                        return;
+
+                    createModel(anchor,arFragment);
+
+                });
+
+
+                break;
+            }
+        }
+    }
+
+
 
     private void loadSoundPool() {
 
@@ -54,36 +106,46 @@ public class MainActivity extends AppCompatActivity {
 
     private void createModel(Anchor anchor, ArFragment arFragment) {
 
+        modelPlaced=true;
+
         ModelRenderable
                 .builder()
-                .setSource(this, Uri.parse("skeletal.sfb"))
+                .setSource(this, Uri.parse("female.sfb"))
                 .build()
                 .thenAccept(modelRenderable -> {
-
+                    renderable = modelRenderable;
                     AnchorNode anchorNode = new AnchorNode(anchor);
-
                     SkeletonNode skeletonNode = new SkeletonNode();
                     skeletonNode.setParent(anchorNode);
-                    skeletonNode.setRenderable(modelRenderable);
-
+                    skeletonNode.setRenderable(renderable);
+                    skeletonNode.setLocalRotation(Quaternion.axisAngle
+                            (new Vector3(0, 1f, 0), 120f));
                     arFragment.getArSceneView().getScene().addChild(anchorNode);
-
-                    Button startBtn = findViewById(R.id.startBtn);
-
-                    startBtn.setOnClickListener(v -> {
-
-                        animateModel(modelRenderable);
-                        soundPool.play(sound, 1f, 1f, 1, 0
-                                , 1f);
-
-                    });
+                    animateModel(renderable);
 
 
 
                 });
+               new Thread(new Runnable() {
+                   @Override
+                   public void run() {
+                       try {
+                           Thread.sleep(4000);
+                           soundPool.play(sound, 1f, 1f, 1, 0
+                                   , 1f);
+                       } catch (InterruptedException e) {
+                           e.printStackTrace();
+                       }
+
+                   }
+               }).start();
+
+
+
 
 
     }
+
 
     private void animateModel(ModelRenderable modelRenderable) {
 
